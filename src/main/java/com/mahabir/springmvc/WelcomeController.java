51fdd7a6053ac8393877a8a8d9607d24b969b191
MahabirGupta <mahabirg81@gmail.com>
2025-12-07 19:59:39 +0800

    add welcome.jsp

---
+++ src/main/java/com/mahabir/springmvc/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.mahabir.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(value="/welcome")
+	public String greeting() {
+		return "welcome";
+	}
+	
+	
+
+}
