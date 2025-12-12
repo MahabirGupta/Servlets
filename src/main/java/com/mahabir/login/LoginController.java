10610c55b0df13ca6db8d9352bf196c9c4067816
MahabirGupta <mahabirg81@gmail.com>
2025-12-12 21:16:13 +0800

    Change URL to http://localhost:8080/login

--- src/main/java/com/mahabir/login/LoginController.java
+++ src/main/java/com/mahabir/login/LoginController.java
@@ -1,12 +1,10 @@
-package com.mahabir.springmvc;
+package com.mahabir.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.mahabir.LoginService;
 
 @Controller
 public class LoginController {
--- src/main/java/com/mahabir/login/LoginService.java
+++ src/main/java/com/mahabir/login/LoginService.java
@@ -1,4 +1,4 @@
-package com.mahabir;
+package com.mahabir.login;
 
 public class LoginService {
 	
