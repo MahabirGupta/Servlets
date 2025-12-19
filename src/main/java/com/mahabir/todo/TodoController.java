453d3dc60a1924de15c7ab3d4079e38c1346f40e
MahabirGupta <mahabirg81@gmail.com>
2025-12-19 21:05:40 +0800

    Make TodoService a @Service and inject it

---
+++ src/main/java/com/mahabir/todo/TodoController.java
@@ -0,0 +1,25 @@
+package com.mahabir.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TodoController {
+	
+//	set the login service
+	@Autowired
+	TodoService todoService;
+	
+	@RequestMapping(value="/list-todos", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model) {
+		model.addAttribute("todos", todoService.retrieveTodos("in28Minutes"));
+		return "list-todos";
+	}
+	
+	
+
+}
